ac5e3f8f94ceef0d5aaa6b69c3a2ce23a06c81b8
Richard Haythornthwaite <richardhaythornthwaite@hotmail.co.uk>
2023-06-05 19:15:55 +0100

    fix plot page

--- pages/Plot.py
+++ pages/Plot.py
@@ -15,7 +15,8 @@ st.title("Data exploration")
 
 flyby_info = st.session_state["flyby_info"]
 
-selected_flyby = st.sidebar.selectbox("Choose training flybys", flyby_info.keys())
+selected_flyby = st.sidebar.selectbox("Choose flyby", flyby_info.keys())
+selected_anode = st.sidebar.selectbox("Choose anode", ["3","4"])
 start_time = st.sidebar.time_input(
     "Choose start time", value=flyby_info[selected_flyby].start_time, help="Only 6 hours loaded at a time"
 )
@@ -23,7 +24,7 @@ end_time = st.sidebar.time_input(
     "Choose end time", value=flyby_info[selected_flyby].end_time, help="Only 6 hours loaded at a time"
 )
 
-df = pd.read_csv(flyby_info[selected_flyby].filepath).drop("Unnamed: 0", axis=1)
+df = pd.read_csv(flyby_info[selected_flyby].anodes[selected_anode].filepath).drop("Unnamed: 0", axis=1)
 df = (
     df.astype({"Time": "datetime64[ms]"})
     .set_index("Time")
